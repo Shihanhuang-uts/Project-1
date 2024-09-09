a3f4e4d868d5cabead47e98e368346d275e02cbe
Shihanhuang-uts <Shihan.huang-1@student.uts.edu.au>
2024-09-09 17:27:03 +1000

    Project-1 App file

---
+++ Project1/Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by Manyue on 9/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
